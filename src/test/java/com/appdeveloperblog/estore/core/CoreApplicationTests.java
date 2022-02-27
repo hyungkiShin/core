3406fe259f4b7d95158a04bf15c0837f26764389
hyungkiShin <tkaqkeldk@nate.com>
2022-02-27 17:58:49 +0900

    [core] add

---
+++ src/test/java/com/appdeveloperblog/estore/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.estore.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
